a40b2951e71bcb0774b83724d83ee270e71712da
Michael Uftring <michael.uftring@gmail.com>
2023-03-21 14:01:48 -0400

    add GetUserById() and some 'get user' tests

--- pkg/queries/pgdb/users.go
+++ pkg/queries/pgdb/users.go
@@ -35,3 +35,32 @@ func (q *Queries) GetByCognitoId(ctx context.Context, id string) (*pgdb.User, er
 		panic(err)
 	}
 }
+
+// GetById returns a user from Postgres based on the user's int id
+// This function also returns the preferred org and whether the user is a super-admin.
+func (q *Queries) GetById(ctx context.Context, id int64) (*pgdb.User, error) {
+
+	queryStr := "SELECT id, node_id, email, first_name, last_name, is_super_admin, preferred_org_id " +
+		"FROM pennsieve.users WHERE id=$1;"
+
+	var user pgdb.User
+	row := q.db.QueryRowContext(ctx, queryStr, id)
+	err := row.Scan(
+		&user.Id,
+		&user.NodeId,
+		&user.Email,
+		&user.FirstName,
+		&user.LastName,
+		&user.IsSuperAdmin,
+		&user.PreferredOrg)
+
+	switch err {
+	case sql.ErrNoRows:
+		fmt.Println("No rows were returned!")
+		return nil, err
+	case nil:
+		return &user, nil
+	default:
+		panic(err)
+	}
+}
---
+++ pkg/queries/pgdb/users_test.go
@@ -0,0 +1,38 @@
+package pgdb
+
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+// TestUsers is the main Test Suite function for Users.
+func TestUsers(t *testing.T) {
+	for scenario, fn := range map[string]func(
+		tt *testing.T, store *SQLStore, orgId int,
+	){
+		"Get User by Id":         testGetUserByID,
+		"Get User by Cognito Id": testGetUserByCognitoId,
+	} {
+		t.Run(scenario, func(t *testing.T) {
+			orgId := 0
+			store := NewSQLStore(testDB[orgId])
+			fn(t, store, orgId)
+		})
+	}
+}
+
+func testGetUserByID(t *testing.T, store *SQLStore, orgId int) {
+	id := int64(1001)
+	user, err := store.GetById(context.TODO(), id)
+	assert.NoError(t, err)
+	assert.Equal(t, user.Id, id)
+}
+
+func testGetUserByCognitoId(t *testing.T, store *SQLStore, orgId int) {
+	id := int64(1002)
+	cognitoId := "22222222-2222-2222-2222-222222222222"
+	user, err := store.GetByCognitoId(context.TODO(), cognitoId)
+	assert.NoError(t, err)
+	assert.Equal(t, user.Id, id)
+}
