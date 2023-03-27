13901dda9c072ce42f66e83eae35a32b88bd7256
Michael Uftring <michael.uftring@gmail.com>
2023-03-27 14:38:17 -0400

    DbPermission: change Collaborate -> Guest

--- pkg/models/pgdb/organizationUserTable.go
+++ pkg/models/pgdb/organizationUserTable.go
@@ -8,7 +8,7 @@ type DbPermission int64
 
 const (
 	NoPermission DbPermission = 0
-	Collaborate               = 1
+	Guest                     = 1
 	Read                      = 2
 	Write                     = 4
 	Delete                    = 8
@@ -20,8 +20,8 @@ func (s DbPermission) String() string {
 	switch s {
 	case NoPermission:
 		return "NoPermission"
-	case Collaborate:
-		return "Collaborate"
+	case Guest:
+		return "Guest"
 	case Read:
 		return "Read"
 	case Write:
@@ -39,8 +39,8 @@ func (s DbPermission) String() string {
 
 type OrganizationUser struct {
 	OrganizationId int64        `json:"organization_id"`
-	UserId       int64        `json:"user_id"`
-	DbPermission DbPermission `json:"permission_bit"`
-	CreatedAt    time.Time    `json:"created_at"`
+	UserId         int64        `json:"user_id"`
+	DbPermission   DbPermission `json:"permission_bit"`
+	CreatedAt      time.Time    `json:"created_at"`
 	UpdatedAt      time.Time    `json:"updated_at"`
 }
