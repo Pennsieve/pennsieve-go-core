5203d25b3c22692d9fd473d3b1f6a10d8a9bd466
Michael Uftring <michael.uftring@gmail.com>
2023-05-24 15:06:07 -0400

    add get org by name and slug, refactor to a common 'get' function with query and args

--- pkg/queries/pgdb/organizations_test.go
+++ pkg/queries/pgdb/organizations_test.go
@@ -15,6 +15,8 @@ func TestOrganizations(t *testing.T) {
 	){
 		"Get Organization by Id":     testGetOrganizationById,
 		"Get Organization by NodeId": testGetOrganizationByNodeId,
+		"Get Organization by Name":   testGetOrganizationByName,
+		"Get Organization by Slug":   testGetOrganizationBySlug,
 	} {
 		t.Run(scenario, func(t *testing.T) {
 			orgId := orgId
@@ -38,4 +40,20 @@ func testGetOrganizationByNodeId(t *testing.T, store *SQLStore, orgId int) {
 	org, err := store.GetOrganizationByNodeId(context.TODO(), nodeId)
 	assert.NoError(t, err)
 	assert.Equal(t, nodeId, org.NodeId)
+}
+
+func testGetOrganizationByName(t *testing.T, store *SQLStore, orgId int) {
+	name := "Ultimate"
+
+	org, err := store.GetOrganizationByName(context.TODO(), name)
+	assert.NoError(t, err)
+	assert.Equal(t, name, org.Name)
+}
+
+func testGetOrganizationBySlug(t *testing.T, store *SQLStore, orgId int) {
+	slug := "ultimate"
+
+	org, err := store.GetOrganizationBySlug(context.TODO(), slug)
+	assert.NoError(t, err)
+	assert.Equal(t, slug, org.Slug)
 }
