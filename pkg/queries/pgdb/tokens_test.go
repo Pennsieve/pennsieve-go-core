98d559f579fb9bf194932d07b29e63616cc8d2a4
Michael Uftring <michael.uftring@gmail.com>
2023-11-09 13:01:39 -0500

    test tokens.GetUserByCognitoId

---
+++ pkg/queries/pgdb/tokens_test.go
@@ -0,0 +1,31 @@
+package pgdb
+
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestTokens(t *testing.T) {
+	for scenario, fn := range map[string]func(
+		tt *testing.T, store *SQLStore, orgId int,
+	){
+		"Get Token User by Cognito Id": testGetTokenUserByCognitoId,
+	} {
+		t.Run(scenario, func(t *testing.T) {
+			orgId := 0
+			store := NewSQLStore(testDB[orgId])
+			fn(t, store, orgId)
+		})
+	}
+}
+
+func testGetTokenUserByCognitoId(t *testing.T, store *SQLStore, orgId int) {
+	userId := int64(2001)
+	organizationId := int64(1)
+	cognitoId := "00000000-1111-0000-2222-000000002001"
+	user, err := store.GetUserByCognitoId(context.TODO(), cognitoId)
+	assert.NoError(t, err)
+	assert.Equal(t, user.Id, userId)
+	assert.Equal(t, user.PreferredOrg, organizationId)
+}
