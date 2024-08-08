83ea2dcab6d0059b176a09664573dd7ec47ceba6
Michael Uftring <michael.uftring@gmail.com>
2024-08-08 06:06:24 -0400

    generate service claim and token

--- pkg/authorizer/claims_test.go
+++ pkg/authorizer/claims_test.go
@@ -1,8 +1,15 @@
 package authorizer
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/pennsieve/pennsieve-go-core/pkg/models/dataset"
+	"github.com/pennsieve/pennsieve-go-core/pkg/models/dataset/role"
+	"github.com/pennsieve/pennsieve-go-core/pkg/models/organization"
+	"github.com/pennsieve/pennsieve-go-core/pkg/models/pgdb"
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 type ClaimResponse struct {
@@ -12,10 +19,13 @@ type ClaimResponse struct {
 func TestClaims(t *testing.T) {
 	for scenario, fn := range map[string]func(
 		tt *testing.T){
-		"Parse Claims":     testParseClaims,
-		"Is Publisher":     testIsPublisher,
-		"Is Not Publisher": testIsNotPublisher,
-		"No Team Claims":   testNoTeamClaims,
+		"Parse Claims":                        testParseClaims,
+		"Is Publisher":                        testIsPublisher,
+		"Is Not Publisher":                    testIsNotPublisher,
+		"No Team Claims":                      testNoTeamClaims,
+		"Generate a Service Claim":            testGenerateServiceClaim,
+		"Generate a Service Claim with roles": testGenerateServiceClaimWithRoles,
+		"Service Claim as Token":              testServiceClaimAsToken,
 	} {
 		t.Run(scenario, func(t *testing.T) {
 			fn(t)
@@ -107,4 +117,51 @@ func testNoTeamClaims(t *testing.T) {
 	assert.NotNil(t, claims.DatasetClaim)
 	assert.NotNil(t, claims.UserClaim)
 	assert.False(t, IsPublisher(claims))
+}
+
+func testGenerateServiceClaim(t *testing.T) {
+	claim := GenerateServiceClaim(5 * time.Minute)
+	assert.NotNil(t, claim)
+}
+
+func testGenerateServiceClaimWithRoles(t *testing.T) {
+	orgClaim := organization.Claim{
+		Role:            pgdb.Owner,
+		IntId:           367,
+		NodeId:          "N:organization:06c8002d-477a-45e9-ae0d-06f4b218628f",
+		EnabledFeatures: nil,
+	}
+	datasetClaim := dataset.Claim{
+		Role:   role.Owner,
+		NodeId: "N:dataset:ca645a17-fb55-4afd-aff8-7e0078b4523f",
+		IntId:  86,
+	}
+	claim := GenerateServiceClaim(5 * time.Minute).WithOrganizationClaim(orgClaim).WithDatasetClaim(datasetClaim)
+	assert.NotNil(t, claim)
+	data, err := json.Marshal(claim)
+	assert.NoError(t, err)
+	dataString := string(data)
+	fmt.Println(dataString)
+}
+
+func testServiceClaimAsToken(t *testing.T) {
+	orgClaim := organization.Claim{
+		Role:            pgdb.Owner,
+		IntId:           367,
+		NodeId:          "N:organization:06c8002d-477a-45e9-ae0d-06f4b218628f",
+		EnabledFeatures: nil,
+	}
+	datasetClaim := dataset.Claim{
+		Role:   role.Owner,
+		NodeId: "N:dataset:ca645a17-fb55-4afd-aff8-7e0078b4523f",
+		IntId:  86,
+	}
+	claim := GenerateServiceClaim(5 * time.Minute).WithOrganizationClaim(orgClaim).WithDatasetClaim(datasetClaim)
+	token, err := claim.AsToken("secret")
+	assert.NoError(t, err)
+	assert.NotNil(t, token)
+	data, err := json.Marshal(token)
+	assert.NoError(t, err)
+	dataString := string(data)
+	fmt.Println(dataString)
 }
--- pkg/authorizer/models/models.go
+++ pkg/authorizer/models/models.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/pennsieve/pennsieve-go-core/pkg/models/dataset"
 	"github.com/pennsieve/pennsieve-go-core/pkg/models/organization"
 	"strconv"
@@ -25,20 +26,43 @@ type ServiceToken struct {
 	Value string `json:"value"`
 }
 
-func (c ServiceClaim) WithOrganizationClaim(claim organization.Claim) {
+func (c ServiceClaim) WithOrganizationClaim(claim organization.Claim) ServiceClaim {
 	c.Roles = append(c.Roles, ServiceRole{
 		Type:   "organization_role",
 		Id:     strconv.FormatInt(claim.IntId, 10),
 		NodeId: claim.NodeId,
 		Role:   claim.Role.AsOrganizationRole(),
 	})
+	return c
 }
 
-func (c ServiceClaim) WithDatasetClaim(claim dataset.Claim) {
+func (c ServiceClaim) WithDatasetClaim(claim dataset.Claim) ServiceClaim {
 	c.Roles = append(c.Roles, ServiceRole{
 		Type:   "dataset_role",
 		Id:     strconv.FormatInt(claim.IntId, 10),
 		NodeId: claim.NodeId,
 		Role:   strings.ToLower(claim.Role.String()),
 	})
+	return c
+}
+
+func (c ServiceClaim) AsToken(key string) (*ServiceToken, error) {
+	var (
+		err          error
+		secret       []byte
+		token        *jwt.Token
+		signedString string
+	)
+	token = jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"iat":   c.IssuedAt,
+		"exp":   c.ExpiresAt,
+		"type":  c.Type,
+		"roles": c.Roles,
+	})
+	secret = []byte(key)
+	signedString, err = token.SignedString(secret)
+	if err != nil {
+		return nil, err
+	}
+	return &ServiceToken{Value: signedString}, nil
 }
