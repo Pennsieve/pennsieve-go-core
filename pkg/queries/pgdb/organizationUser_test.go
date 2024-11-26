8cc57cd2e7042dd869a2ec9238df766c2a27fd6a
John Frommeyer <johnfr@seas.upenn.edu>
2024-11-26 15:31:35 -0500

    New GetOrganizationClaim tests

--- pkg/queries/pgdb/organizationUser_test.go
+++ pkg/queries/pgdb/organizationUser_test.go
@@ -2,8 +2,10 @@ package pgdb
 
 import (
 	"context"
+	"fmt"
 	"github.com/pennsieve/pennsieve-go-core/pkg/models/pgdb"
 	"github.com/stretchr/testify/assert"
+	"slices"
 	"testing"
 )
 
@@ -11,11 +13,14 @@ func TestOrganizationUser(t *testing.T) {
 	for scenario, fn := range map[string]func(
 		tt *testing.T, store *SQLStore, orgId int,
 	){
-		"Get Organization User":           testGetOrganizationUser,
-		"Add Organization User":           testAddOrganizationUser,
-		"Add Existing OrgUser Membership": testAddExistingOrgUserMembership,
-		"Add Guest to Organization":       testAddGuestToOrganization,
-		"Get Organization Claim":          testGetOrganizationClaim,
+		"Get Organization User":                                  testGetOrganizationUser,
+		"Add Organization User":                                  testAddOrganizationUser,
+		"Add Existing OrgUser Membership":                        testAddExistingOrgUserMembership,
+		"Add Guest to Organization":                              testAddGuestToOrganization,
+		"Get Organization Claim":                                 testGetOrganizationClaim,
+		"Get Organization Claim error when no org user":          testGetOrganizationClaimNoOrgUser,
+		"Get Organization Claim when org has no feature flags":   testGetOrganizationClaimNoFeatureFlags,
+		"Get Organization Claim when org has many feature flags": testGetOrganizationClaimManyFeatureFlags,
 	} {
 		t.Run(scenario, func(t *testing.T) {
 			orgId := 1
@@ -86,4 +91,68 @@ func testGetOrganizationClaim(t *testing.T, store *SQLStore, orgId int) {
 	assert.NoError(t, err)
 	assert.Equal(t, organizationId, orgClaim.IntId)
 	assert.Equal(t, org.NodeId, orgClaim.NodeId)
+	assert.Equal(t, pgdb.Delete, orgClaim.Role)
+	// org 1 is the sandbox org created by migrations in the seed DB.
+	// It has one feature flag
+	assert.Len(t, orgClaim.EnabledFeatures, 1)
+	assert.Equal(t, "sandbox_org_feature", orgClaim.EnabledFeatures[0].Feature)
+	assert.Equal(t, organizationId, orgClaim.EnabledFeatures[0].OrganizationId)
+	assert.True(t, orgClaim.EnabledFeatures[0].Enabled)
+}
+
+func testGetOrganizationClaimNoOrgUser(t *testing.T, store *SQLStore, _ int) {
+	// store_test.go and tests in this file do not add user 1002 to org 1
+	userId := int64(1002)
+	organizationId := int64(1)
+
+	_, err := store.GetOrganizationClaim(context.TODO(), userId, organizationId)
+	assert.ErrorContains(t, err, "organization user was not found")
+}
+
+func testGetOrganizationClaimNoFeatureFlags(t *testing.T, store *SQLStore, orgId int) {
+	userId := int64(1002)
+	organizationId := int64(2)
+
+	// get the organization so that we can check NodeId
+	org, err := store.GetOrganization(context.TODO(), organizationId)
+	assert.NoError(t, err)
+	assert.Equal(t, organizationId, org.Id)
+
+	// get the organization claim
+	orgClaim, err := store.GetOrganizationClaim(context.TODO(), userId, organizationId)
+	assert.NoError(t, err)
+	assert.Equal(t, organizationId, orgClaim.IntId)
+	assert.Equal(t, org.NodeId, orgClaim.NodeId)
+	assert.Equal(t, pgdb.Delete, orgClaim.Role)
+	assert.Empty(t, orgClaim.EnabledFeatures)
+}
+
+func testGetOrganizationClaimManyFeatureFlags(t *testing.T, store *SQLStore, orgId int) {
+	userId := int64(3402)
+	organizationId := int64(402)
+
+	// get the organization so that we can check NodeId
+	org, err := store.GetOrganization(context.TODO(), organizationId)
+	assert.NoError(t, err)
+	assert.Equal(t, organizationId, org.Id)
+
+	// get the organization claim
+	orgClaim, err := store.GetOrganizationClaim(context.TODO(), userId, organizationId)
+	assert.NoError(t, err)
+	assert.Equal(t, organizationId, orgClaim.IntId)
+	assert.Equal(t, org.NodeId, orgClaim.NodeId)
+	assert.Equal(t, pgdb.Read, orgClaim.Role)
+	assert.Len(t, orgClaim.EnabledFeatures, 5)
+	disabledIndex := slices.IndexFunc(orgClaim.EnabledFeatures, func(flag pgdb.FeatureFlags) bool {
+		return flag.Feature == "disabled feature" && flag.Enabled == false
+	})
+	assert.True(t, disabledIndex >= 0, "expected disabled feature not found in %s", orgClaim.EnabledFeatures)
+	enabledFeatures := []string{"one", "two", "three", "four"}
+	for _, enabledFeature := range enabledFeatures {
+		index := slices.IndexFunc(orgClaim.EnabledFeatures, func(flag pgdb.FeatureFlags) bool {
+			return flag.Enabled && flag.Feature == fmt.Sprintf("feature %s", enabledFeature)
+		})
+		assert.True(t, index >= 0, "expected enabled feature %s not found in %s", enabledFeature, orgClaim.EnabledFeatures)
+
+	}
 }
