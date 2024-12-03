70471a81e2212fa1ae39c9c314b4f472baa0601b
John Frommeyer <johnfr@seas.upenn.edu>
2024-12-03 15:53:26 -0500

    Unifying org claim construction with a shared helper method and types.

--- pkg/queries/pgdb/organizationUser.go
+++ pkg/queries/pgdb/organizationUser.go
@@ -100,39 +100,17 @@ func (q *Queries) AddOrganizationUser(ctx context.Context, orgId int64, userId i
 	return orgUser, nil
 }
 
-// GetOrganizationClaim returns an organization claim for a specific user.
+// GetOrganizationClaim returns an organization claim for a specific user given a workspace id
 func (q *Queries) GetOrganizationClaim(ctx context.Context, userId int64, organizationId int64) (*organization.Claim, error) {
-
-	currentOrgUser, err := q.GetOrganizationUser(ctx, organizationId, userId)
-	if err != nil {
-		log.Error("Unable to check Org User: ", err)
-		return nil, err
-	}
-
-	org, err := q.GetOrganization(ctx, organizationId)
-	if err != nil {
-		log.Error("Unable to check Organization: ", err)
-		return nil, err
-	}
-
-	enabledFeatures, err := q.GetEnabledFeatureFlags(ctx, organizationId)
-	if err != nil {
-		log.Error("Unable to check Feature Flags: ", err)
-		return nil, err
-	}
-
-	orgRole := organization.Claim{
-		Role:            currentOrgUser.DbPermission,
-		IntId:           organizationId,
-		NodeId:          org.NodeId,
-		EnabledFeatures: enabledFeatures,
-	}
-
-	return &orgRole, nil
-
+	return queryOrganizationClaim(ctx, q.db, &orgClaimByOrgId, userId, organizationId)
 }
 
-// nullableFeatureFlag is a temp struct used to hold results from the query used by GetOrganizationClaimByNodeId which
+// GetOrganizationClaimByNodeId returns an organization claim for a specific user given a workspace node id
+func (q *Queries) GetOrganizationClaimByNodeId(ctx context.Context, userId int64, organizationNodeId string) (*organization.Claim, error) {
+	return queryOrganizationClaim(ctx, q.db, &orgClaimByOrgNodeId, userId, organizationNodeId)
+}
+
+// nullableFeatureFlag is a temp struct used to hold results from the query used by queryOrganizationClaim which
 // will return rows with null values for featureFlag columns if the org has no flags set
 type nullableFeatureFlag struct {
 	feature   sql.NullString
@@ -141,7 +119,7 @@ type nullableFeatureFlag struct {
 	updatedAt sql.NullTime
 }
 
-func (n nullableFeatureFlag) Valid() bool {
+func (n nullableFeatureFlag) valid() bool {
 	return n.feature.Valid && n.enabled.Valid && n.createdAt.Valid && n.updatedAt.Valid
 }
 
@@ -155,57 +133,90 @@ func (n nullableFeatureFlag) toFeatureFlag(organizationId int64) pgdb.FeatureFla
 	}
 }
 
-func (q *Queries) GetOrganizationClaimByNodeId(ctx context.Context, userId int64, organizationNodeId string) (*organization.Claim, error) {
-	query := `SELECT o.id, ou.permission_bit, f.feature, f.enabled, f.created_at, f.updated_at 
-			  FROM pennsieve.users u JOIN pennsieve.organization_user ou ON u.id = ou.user_id
-         			                 JOIN pennsieve.organizations o ON ou.organization_id = o.id
-         			            LEFT JOIN pennsieve.feature_flags f ON o.id = f.organization_id and f.enabled = true
-			  WHERE u.id = $1 AND o.node_id = $2`
+type orgClaimQuery struct {
+	name            string
+	query           string
+	paramsMsgFormat string
+}
 
-	rows, err := q.db.QueryContext(ctx, query, userId, organizationNodeId)
+func (q *orgClaimQuery) String() string {
+	return q.name
+}
+
+func (q *orgClaimQuery) paramsMsg(userId int64, orgIdentifier any) string {
+	return fmt.Sprintf(q.paramsMsgFormat, userId, orgIdentifier)
+}
+
+const orgClaimQueryFormat = `SELECT o.id, o.node_id, ou.permission_bit, f.feature, f.enabled, f.created_at, f.updated_at 
+			  		         FROM pennsieve.users u JOIN pennsieve.organization_user ou ON u.id = ou.user_id
+         			             			        JOIN pennsieve.organizations o ON ou.organization_id = o.id
+         			                           LEFT JOIN pennsieve.feature_flags f ON o.id = f.organization_id and f.enabled = true
+			                 WHERE u.id = $1 AND %s = $2`
+
+// orgClaimByOrgNodeId is the query to get an org claim given a user id and workspace node id
+var orgClaimByOrgNodeId = orgClaimQuery{
+	name:            "GetOrganizationClaimByNodeId",
+	query:           fmt.Sprintf(orgClaimQueryFormat, "o.node_id"),
+	paramsMsgFormat: "user id: %d, workspace node id: %s",
+}
+
+// orgClaimByOrgId is the query to get an org claim given a user id and workspace (int) id
+var orgClaimByOrgId = orgClaimQuery{
+	name:            "GetOrganizationClaim",
+	query:           fmt.Sprintf(orgClaimQueryFormat, "o.id"),
+	paramsMsgFormat: "user id: %d, workspace id: %d",
+}
+
+// queryOrganizationClaim constructs an org claim from the given query and params. It assumes that the given query
+// is like orgClaimQueryFormat: it has those columns in that order, and has two positional params. $1 is the user id
+// and $2 is the org identifier, either the int or node id of the workspace.
+func queryOrganizationClaim(ctx context.Context, db DBTX, orgClaimQuery *orgClaimQuery, userId int64, orgIdentifier any) (*organization.Claim, error) {
+	rows, err := db.QueryContext(ctx, orgClaimQuery.query, userId, orgIdentifier)
 	if err != nil {
-		return nil, fmt.Errorf("error getting organization claim by node id: %w", err)
+		return nil, fmt.Errorf("error getting organization claim with query %s: %w", orgClaimQuery, err)
 	}
 	defer func() {
 		if err := rows.Close(); err != nil {
-			log.Warn("error closing rows for get organization claim by node id", err)
+			log.Warn("error closing rows for get organization claim with query", orgClaimQuery, "error:", err)
 		}
 	}()
 
-	// orgId and orgPerms may get multiple scans below (if the org has >1 feature flag) but should always be the same value. Not
+	// orgId, orgNodeId, and orgPerms may get multiple scans below (if the org has >1 feature flag) but should always be the same value. Not
 	// sure how else to handle this except with the redundant scans.
 	var orgId int64
+	var orgNodeId string
 	var orgPerms pgdb.DbPermission
 	var flags []pgdb.FeatureFlags
 	for rows.Next() {
 		var nullableFlag nullableFeatureFlag
 		if err := rows.Scan(
 			&orgId,
+			&orgNodeId,
 			&orgPerms,
 			&nullableFlag.feature,
 			&nullableFlag.enabled,
 			&nullableFlag.createdAt,
 			&nullableFlag.updatedAt); err != nil {
-			return nil, fmt.Errorf("error reading row for get organization claim by node id: %w", err)
+			return nil, fmt.Errorf("error reading row for get organization claim with query %s: %w", orgClaimQuery, err)
 		}
 		// an org may have no feature flags. don't add a bunch of zero-ed structs to claim
-		if nullableFlag.Valid() {
+		if nullableFlag.valid() {
 			featureFlag := nullableFlag.toFeatureFlag(orgId)
 			flags = append(flags, featureFlag)
 		}
 	}
 	if err := rows.Err(); err != nil {
-		return nil, fmt.Errorf("row error on get organization claim by node id: %w", err)
+		return nil, fmt.Errorf("row error on get organization claim with query %s: %w", orgClaimQuery, err)
 	}
 	// zero orgId means no rows returned.
 	if orgId == 0 {
-		return nil, OrganizationUserNotFoundError{fmt.Sprintf("user id: %d, organization node id: %s", userId, organizationNodeId)}
+		return nil, OrganizationUserNotFoundError{orgClaimQuery.paramsMsg(userId, orgIdentifier)}
 	}
 
 	return &organization.Claim{
 		Role:            orgPerms,
 		IntId:           orgId,
-		NodeId:          organizationNodeId,
+		NodeId:          orgNodeId,
 		EnabledFeatures: flags,
 	}, nil
 }
