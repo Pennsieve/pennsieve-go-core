8385319fc8f400cc79e2dbcea125b7592ffd66cf
John Frommeyer <johnfr@seas.upenn.edu>
2026-03-05 14:27:51 -0500

    Update pennsievedb version to get files.published_s3_version_id. Remove File.Published. Add File.PublishedS3VersionId.

--- pkg/models/pgdb/fileTable.go
+++ pkg/models/pgdb/fileTable.go
@@ -10,21 +10,21 @@ import (
 )
 
 type File struct {
-	Id              string                          `json:"id"`
-	PackageId       int                             `json:"package_id"`
-	Name            string                          `json:"name"`
-	FileType        fileType.Type                   `json:"file_type"`
-	S3Bucket        string                          `json:"s3_bucket"`
-	S3Key           string                          `json:"s3_key"`
-	ObjectType      objectType.ObjectType           `json:"object_type"`
-	Size            int64                           `json:"size"`
-	CheckSum        string                          `json:"checksum"`
-	UUID            uuid.UUID                       `json:"uuid"`
-	ProcessingState processingState.ProcessingState `json:"processing_state"`
-	UploadedState   uploadState.UploadedState       `json:"uploaded_state"`
-	CreatedAt       time.Time                       `json:"created_at"`
-	UpdatedAt       time.Time                       `json:"updated_at"`
-	Published       bool                            `json:"published"`
+	Id                   string                          `json:"id"`
+	PackageId            int                             `json:"package_id"`
+	Name                 string                          `json:"name"`
+	FileType             fileType.Type                   `json:"file_type"`
+	S3Bucket             string                          `json:"s3_bucket"`
+	S3Key                string                          `json:"s3_key"`
+	ObjectType           objectType.ObjectType           `json:"object_type"`
+	Size                 int64                           `json:"size"`
+	CheckSum             string                          `json:"checksum"`
+	UUID                 uuid.UUID                       `json:"uuid"`
+	ProcessingState      processingState.ProcessingState `json:"processing_state"`
+	UploadedState        uploadState.UploadedState       `json:"uploaded_state"`
+	CreatedAt            time.Time                       `json:"created_at"`
+	UpdatedAt            time.Time                       `json:"updated_at"`
+	PublishedS3VersionId *string                         `json:"published_s3_version_id"`
 }
 
 type FileParams struct {
